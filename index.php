d9da93702c252e850674bb714a7a38bb6cbe8fc4
Stepan Sotnikov <skewlife@gmail.com>
2016-07-05 22:43:45 +0900

    initialization

---
+++ index.php
@@ -0,0 +1,54 @@
+﻿<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Welcome</title>
+  <meta name="description" content="Website space66" />
+  <meta name="keywords" content="space, website" />
+  <meta name="author" content="Scube" />
+  <link rel="shortcut icon" href="../favicon.ico">
+  <link rel="stylesheet" type="text/css" href="" />
+</head>
+
+<body>
+  <!--Container-->
+  <div class="container">
+    <div class="registrationArea">
+      <div class="logo">
+      </div>
+      <div class="email">
+      </div>
+      <div class="password">
+      </div>
+      <div class="submit">
+      </div>
+    </div>
+  </div>
+  <!--Footer-->
+  <form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputFile">File input</label>
+        <input type="file" id="exampleInputFile">
+        <p class="help-block">Example block-level help text here.</p>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox">Check me out
+        </label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+</form>
+  <footer>
+  </footer>
+</body>
+</html>
